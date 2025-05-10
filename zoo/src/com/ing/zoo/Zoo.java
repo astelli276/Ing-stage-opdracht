767d212874736c99604e30e40c2b2cf8f626cf78
Astrid <astrid.de.vries@hva.nl>
2025-05-10 02:46:39 +0200

    refactor zoo

--- zoo/src/com/ing/zoo/Zoo.java
+++ zoo/src/com/ing/zoo/Zoo.java
@@ -1,17 +1,19 @@
 package com.ing.zoo;
 
 import com.ing.zoo.animal.*;
+import com.ing.zoo.commands.*;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Zoo {
     public static void main(String[] args)
     {
-        String[] commands = new String[4];
-        commands[0] = "hello";
-        commands[1] = "give leaves";
-        commands[2] = "give meat";
-        commands[3] = "perform trick";
+        Map<String, GiveCommand> commandMap = new HashMap<>();
+        commandMap.put("hello", new HelloCommand());
+        commandMap.put("give meat", new EatmeatCommand());
+        commandMap.put("give leaves", new EatLeavesCommand());
+        commandMap.put("perform trick", new PerformTrickCommand());
+        commandMap.put("quit", new QuitCommand());
 
         Lion henk = new Lion("henk");
         Hippo elsa = new Hippo("elsa");
@@ -19,17 +21,22 @@ public class Zoo {
         Tiger wally = new Tiger("wally");
         Zebra marty = new Zebra("marty");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Voer uw command in: ");
+        List<Animal> animals = Arrays.asList(henk, elsa, dora, wally, marty);
 
-        String input = scanner.nextLine();
-        if(input.equals(commands[0] + " henk"))
-        {
-            henk.sayHello();
-        }
-        else
-        {
-            System.out.println("Unknown command: " + input);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("\nVoer uw command in: ");
+            String input = scanner.nextLine();
+            String[] parts = input.split(" ", 2);
+            String commandKey = parts[0].equals("hello") && parts.length > 1 ? "hello" : input.trim();
+            String argument = parts.length > 1 ? parts[1] : null;
+
+            GiveCommand command = commandMap.get(commandKey);
+            if (command != null) {
+                command.execute(animals, argument);
+            } else {
+                System.out.println("Unknown command: " + input);
+            }
         }
     }
 }
